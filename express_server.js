97d402b2ba0eb912edb6da50f430f3385c6de099
Kiup Kim <kukim96@gmail.com>
2021-04-13 22:46:50 -0600

    refactored to work

--- express_server.js
+++ express_server.js
@@ -85,7 +85,7 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/register", (req, res) => {
+app.get("/register", (req, res) => {
   const templateVars = {username: req.cookies["username"]};
   res.render("urls_registration", templateVars);
 });
