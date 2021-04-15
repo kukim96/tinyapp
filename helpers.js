41a97966a243c43fa292c7ac204b66e28989d7ff
Kiup Kim <kukim96@gmail.com>
2021-04-15 15:09:31 -0600

    move getUserbyEmail

---
+++ helpers.js
@@ -0,0 +1,9 @@
+const getUserByEmail = (email, database) => {
+  for (const user in database) {
+    if(database[user].email === email) {
+      return database[user];
+    }
+  } return undefined;
+};
+
+module.exports = { getUserByEmail };
