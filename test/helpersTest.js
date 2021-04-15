f936f0c46a1b4d57dbe045ac81566116459c1f1e
Kiup Kim <kukim96@gmail.com>
2021-04-15 15:49:35 -0600

    created test

---
+++ test/helpersTest.js
@@ -0,0 +1,30 @@
+const { assert } = require("chai");
+
+const { getUserByEmail } = require("../helpers");
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('#getUserByEmail', () => {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedOutput = "userRandomID";
+    assert.equal(user, testUsers.userRandomID);
+  });
+  
+  it('should return undefined for non-existent email', () => {
+    const user = getUserByEmail("iLoveCoding@code.com", testUsers)
+    const expectedOutput = undefined;
+    assert.equal(user, undefined);
+  })
+});
